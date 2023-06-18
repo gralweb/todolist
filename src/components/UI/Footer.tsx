3d75faf27784c0c227da28cbcbe10e1b94963a96
gralweb <gralweb21@gmail.com>
2023-06-18 16:51:15 -0400

    feat(app): Creacion de estructura y estilos para el Footer de la app

---
+++ src/components/UI/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+	return (
+		<>
+			<section className="w-11/12 max-w-3xl mx-auto mt-5 pb-20 flex place-content-center">
+				<a 
+					href="https://gralweb.web.app" target="_blank" rel="noopener noreferrer"
+					className="text-black text-center text-xl font-black uppercase tracking-wide"
+				>
+					<h1>gralweb</h1>
+				</a>
+			</section>
+		</>
+	)
+}
+
+export default Footer
