b8610e5f19798f8142f08c8295b7cc32dcbbcba3
gralweb <gralweb21@gmail.com>
2023-06-18 17:02:48 -0400

    feat(app): Estructura, estilos de la vista cuando no hay todos creados

---
+++ src/components/Todo/ViewVoidTodo/index.tsx
@@ -0,0 +1,14 @@
+const ViewVoidTodo = () => {
+	return (
+		<>
+			<section className="w-11/12 max-w-3xl grid place-items-center h-screen max-h-80 bg-white p-10 mx-auto my-6 border-2 rounded border-black">
+				<h1 className="text-center text-lg uppercase font-medium">
+					Te invito a crear tu primer tarea dandole click al boton 
+					<span className="text-2xl ml-1">+</span>
+				</h1>
+			</section>
+		</>
+	)
+}
+
+export default ViewVoidTodo
