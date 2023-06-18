1517afbb7d6f454f104ea5219cf9411ee62ef54a
gralweb <gralweb21@gmail.com>
2023-06-18 16:47:17 -0400

    feat(app): Creacion de estructura y estilos para el header de la app

---
+++ src/components/UI/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+	return (
+		<>
+			<section className="w-11/12 max-w-3xl h-40 mx-auto p-5 rounded-b-lg grid place-content-center border-x-2 border-b-2 border-black">
+				<h1 className="uppercase font-bold text-4xl text-black">
+					Todo-List
+				</h1>
+			</section>
+		</>
+	)
+}
+
+export default Header
