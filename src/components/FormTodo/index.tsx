771773c73c5ac69dbc07763d97b60d15d5446ed5
gralweb <gralweb21@gmail.com>
2023-06-19 12:23:39 -0400

    refactor(app): mejora de la presentacion de props para una mejor lectura del codigo

--- src/components/FormTodo/index.tsx
+++ src/components/FormTodo/index.tsx
@@ -4,13 +4,14 @@ import ButtonForm from "./ButtonForm"
 import { v4 as uuidv4 } from 'uuid'
 import { Todo } from "../../interfaces/todo"
 
-const FormTodo = (props: {
-		title: string, title_todo?: string, content_todo?: string,
-		id_todo?: string, check_todo?: boolean, delete_todo?: boolean,
-		setTodo: Function, handlerVisibility: Function
-	}) => {
-	
-	const { title, title_todo, content_todo, id_todo, check_todo, delete_todo, setTodo, handlerVisibility } = props
+type FormTodoProps = {
+	title:string,
+	todo?: Todo,
+	handlerForm: (formData: Todo) => void,
+	handlerVisibility: Function
+}
+
+const FormTodo: React.FC<FormTodoProps> = ({title, todo, handlerForm, handlerVisibility}) => {
 	const [todoForm, setTodoForm] = useState<Todo>({
 		id: '',
 		title: '',
@@ -20,7 +21,7 @@ const FormTodo = (props: {
 	})
 
 	// Manejar el envio del formulario de creacion y edicion
-	const handlerForm = (e: FormEvent<HTMLFormElement>) => {
+	const handlerFormAdd = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault()
 
 		const target = e.target as HTMLFormElement
@@ -39,29 +40,29 @@ const FormTodo = (props: {
 	useEffect(() => {
 		// Comprobar si se han envido los datos del formulario para guardar
 		if (todoForm.title !== '') {
-			setTodo(todoForm)
+			handlerForm(todoForm)
 			handlerVisibility(false)
 		}
 	}, [todoForm])
 
 	return (
 		<>
-			<form onSubmit={handlerForm} className="w-11/12 max-w-3xl fixed top-1/4 z-10 left-0 right-0 bg-white overflow-hidden p-10 mx-auto my-6 border-2 rounded border-black">
+			<form onSubmit={handlerFormAdd} className="w-11/12 max-w-3xl fixed top-1/4 z-10 left-0 right-0 bg-white overflow-hidden p-10 mx-auto my-6 border-2 rounded border-black">
 				<div className="text-black tracking-wide font-bold mb-4">
 					<h1 className="text-xl">{title} TODO</h1>
 				</div>
 
 				<div className="mb-6">
-					<InputForm type='text' label='titulo' holder='Titulo para TODO' value={title_todo} />
+					<InputForm type='text' label='titulo' holder='Titulo para TODO' value={todo?.title} />
 				</div>
 
 				<div className="mb-6">
-					<InputForm type='textarea' label='contenido' holder='Contenido para TODO' value={content_todo} />
+					<InputForm type='textarea' label='contenido' holder='Contenido para TODO' value={todo?.content} />
 				</div>
 
-				<InputForm type='hidden' label='id_todo' value={!id_todo ? uuidv4() : id_todo} />
-				<InputForm type='hidden' label='check_todo' value={JSON.stringify(check_todo) || "false"} />
-				<InputForm type='hidden' label='delete_todo' value={JSON.stringify(delete_todo) || "false"} />
+				<InputForm type='hidden' label='id_todo' value={!todo?.id ? uuidv4() : todo?.id} />
+				<InputForm type='hidden' label='check_todo' value={JSON.stringify(todo?.check) || "false"} />
+				<InputForm type='hidden' label='delete_todo' value={JSON.stringify(todo?.delete) || "false"} />
 				
 				<div className="gap-4 flex justify-between">
 					<div onClick={() => handlerVisibility(false)} className="w-full">
