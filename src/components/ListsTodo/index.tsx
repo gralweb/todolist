a84b5811810486c9027f41343bf4609b02ff514a
gralweb <gralweb21@gmail.com>
2023-06-19 12:23:50 -0400

    refactor(app): mejora de la presentacion de props para una mejor lectura del codigo

--- src/components/ListsTodo/index.tsx
+++ src/components/ListsTodo/index.tsx
@@ -1,14 +1,16 @@
 import ListTodo from "./ListTodo"
-import { Todo } from '../../interfaces/todo'
 
-const ListsTodo = (props: { todos: Array<Todo>, setTodos: Function, setTodoId: Function, setFormEditTodoOpenState: Function }) => {
-	const {todos, setTodos, setFormEditTodoOpenState, setTodoId} = props
+type ListsTodoProps = {
+	setTodoId: Function,
+	setFormEditTodoOpenState: Function
+}
 
+const ListsTodo: React.FC<ListsTodoProps> = ({ setTodoId, setFormEditTodoOpenState }) => {
 	return (
 		<>
 			<section className="w-11/12 max-w-3xl border-2 mx-auto my-6 p-10  border-black rounded md:flex grid gap-3" >
-				<ListTodo title='Completar' check={false} todos={todos} setTodos={setTodos} setTodoId={setTodoId} setFormEditTodoOpenState={setFormEditTodoOpenState} />
-				<ListTodo title='Completado' check={true} todos={todos} setTodos={setTodos} setTodoId={setTodoId} setFormEditTodoOpenState={setFormEditTodoOpenState} />
+				<ListTodo title='Completar' check={false} setTodoId={setTodoId} setFormEditTodoOpenState={setFormEditTodoOpenState} />
+				<ListTodo title='Completado' check={true} setTodoId={setTodoId} setFormEditTodoOpenState={setFormEditTodoOpenState} />
 			</section>
 		</>
 	)
